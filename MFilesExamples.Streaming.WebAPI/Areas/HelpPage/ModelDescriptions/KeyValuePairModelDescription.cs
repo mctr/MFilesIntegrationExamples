033cac5328621f3030ee4da126c7777ef8991037
Fırat Öztürk <ozturk.yf@gmail.com>
2016-05-05 12:04:59 +0300

    Web API Added

---
+++ MFilesExamples.Streaming.WebAPI/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace MFilesExamples.Streaming.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ MFilesExamples.Streaming.WebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MFilesExamples.Streaming.WebAPI.WebApiApplication" Language="C#" %>
